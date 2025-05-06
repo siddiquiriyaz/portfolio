ac86a52eda7b268f3193a1e895cf6b02203147b1
siddiquiriyaz <siddiquiriyaz412@gmail.com>
2025-05-06 19:30:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-const swiper = new Swiper('.swiper', {
-  loop: true,
-  pagination: {
-    el: '.swiper-pagination',
-  },
+
+document.addEventListener("DOMContentLoaded", () => {
+  const bars = document.querySelectorAll(".fill");
+  bars.forEach(bar => {
+    const width = bar.style.width;
+    bar.style.width = "0";
+    setTimeout(() => {
+      bar.style.width = width;
+    }, 300);
+  });
 });
